b8f42f81bc3c4a4468bf6834a5da32f09799744e
bsao17 <greta.b.meddhed>
2020-10-05 10:12:19 +0200

    Add Template html

--- appTwo/views.py
+++ appTwo/views.py
@@ -7,12 +7,19 @@ def djangorocks(request):
 
 def picture_details(request, category, year=0, month=0, day=0):
     template = loader.get_template('appTwo/index.html')
+
+    picture = {
+        'filename': 'mountain.jpg',
+        'category': ['color', 'landscape'],
+    }
+
     context = {
         'title':'Here is the Pictures Details !',
         'category': category,
         'year': year,
         'month': month,
-        'day': day
+        'day': day,
+        'picture': picture,
 
     }
     return HttpResponse(template.render(context, request))
